a07ddd84d470d78322b8903dcf8a9f1ba8bc9d0a
EVE <eve@example.com>
2023-05-27 23:29:57 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//root handler
+app.get('/',(req,res)=>{
+    res.send({'hello':'there'}) //display a simple json
+}
+);
+
+//dynamic port binding
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
