7b6677cc6358ac7dad1bf7ef13c8bd19e3163390
EVE <eve@example.com>
2023-06-07 01:08:33 +0800

    local test dialogflow api send text ok

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,20 +1,34 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const config = require('../config/keys');
 const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+const sessionPath = sessionClient.projectAgentSessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
 
 module.exports = app => {
     //root handler
-app.get('/',(req,res)=>{
-    res.send({'hello':'there'}) //display a simple json
-});
+    app.get('/', (req, res) => {
+        res.send({'hello': 'Johnny'})
+    });
+//dialogflow API - text query
+    app.post('/api/df_text_query', async (req, res) => {
 
-app.post('/api/df_text_query',(req,res)=>{
-    res.send({'do':'text query'}) //api text query placeholder
-});
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: req.body.text,
+                    languageCode: config.dialogFlowSessionLanguageCode
+                }
+            }
+        };
+        let responses = await sessionClient
+            .detectIntent(request);
 
-app.post('/api/df_event_query',(req,res)=>{
-    res.send({'do':'event query'}) //api event query placeholder
-});
+        res.send(responses[0].queryResult)
+    });
+
+    //dialogflow API - placeholder for event query
+    app.post('/api/df_event_query', (req, res) => {
+        res.send({'do': 'event query'})
+    });
 }
